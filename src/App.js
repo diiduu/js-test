f0c9345a9102176b5fced383dc9c30b60ee37ccc
diiduu <diiduu@naver.com>
2022-11-22 14:12:37 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import { Fragment } from "react";
 
 function App() {
+  const name = "홍지수";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>{name} 안녕</h1>
+      <h2>hello </h2>
+    </Fragment>
   );
 }
 
